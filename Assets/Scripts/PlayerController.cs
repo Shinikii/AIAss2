5ca3d307d9cd1d24953b7426d1ea01aa4172426e
Shinikii <billmingko@yahoo.com>
2020-02-01 19:07:40 -0500

    Player and Playermovement
    orcy boi do walk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+// PlayerScript requires the GameObject to have a Rigidbody2D component
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float playerSpeed = 50f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+    }
+}
