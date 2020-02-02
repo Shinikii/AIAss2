43db8e82d7e6ef13ba1f251ac210431ae04fa519
Shinikii <billmingko@yahoo.com>
2020-02-01 22:39:10 -0500

    Particles and fixes
    yeap...

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door
+public class Door : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool isSafe;
@@ -70,5 +70,13 @@ public class Door
         return isOpen;
     }
 
-    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
 }
